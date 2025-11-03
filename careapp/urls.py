bf824fbe7d951d6f585eaa28c4076ce4a9c87074
keithmarvine <mwegakeith91@gmail.com>
2025-11-03 08:08:13 +0300

    loginregister

--- careapp/urls.py
+++ careapp/urls.py
@@ -5,17 +5,20 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name= 'home'),
-    path('starter', views.starter, name= 'starter'),
-    path('about', views.about, name = 'about'),
-    path('services', views.services, name = 'services'),
-    path('departments', views.departments, name = 'departments'),
-    path('appointments', views.appointments, name = 'appointments'),
-    path('doctors', views.doctors, name = 'doctors'),
-    path('contact', views.contact, name = 'contact'),
-    path('show', views.show, name = 'show'),
+    path('home/', views.home, name= 'home'),
+    path('starter/', views.starter, name= 'starter'),
+    path('about/', views.about, name = 'about'),
+    path('services/', views.services, name = 'services'),
+    path('departments/', views.departments, name = 'departments'),
+    path('appointments/', views.appointments, name = 'appointments'),
+    path('doctors/', views.doctors, name = 'doctors'),
+    path('contact/', views.contact, name = 'contact'),
+    path('show/', views.show, name = 'show'),
     path('delete/<int:id>', views.delete),
-    path('edit/<int:id>', views.edit)
+    path('edit/<int:id>', views.edit),
+    path('', views.register, name = 'register'),
+    path('login/', views.login_view, name = 'login'),
+
 
 
 ]
