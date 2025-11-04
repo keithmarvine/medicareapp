0b23d63893a26efc3cc94e9f98b6130082de91d2
keithmarvine <mwegakeith91@gmail.com>
2025-11-04 10:31:08 +0300

    mysqldatabase

--- medipro/settings.py
+++ medipro/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'medipro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'medilab',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
